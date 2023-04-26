864765a31ebafee13c7493808ce31210328d5b32
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-04-26 20:25:18 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {  contactsSlice } from './contactsSlice';
-import {filterSlice} from './filterSlice'
+import { contactsSlice, contactsSliceBuilder } from './contactsSlice';
+import { filterSlice } from './filterSlice';
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
+    // contacts: contactsSlice.reducer,
+    contacts: contactsSliceBuilder.reducer,
     filter: filterSlice.reducer,
   },
- 
 });
 
 
